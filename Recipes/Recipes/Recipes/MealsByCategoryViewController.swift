83d356ae346bc3071d29051bc3e7c2f1b82e1f76
FiodarShtytsko <134956650+FiodarShtytsko@users.noreply.github.com>
2024-03-14 18:16:16 +0100

    add async download

--- Recipes/Recipes/Recipes/MealsByCategoryViewController.swift
+++ Recipes/Recipes/Recipes/MealsByCategoryViewController.swift
@@ -16,6 +16,7 @@ final class MealsByCategoryViewController: UIViewController {
             tableView.reloadData()
         }
     }
+  private var images: [Int: UIImage] = [:]
     @IBOutlet private weak var addedToFavoriteButton: UIBarButtonItem!
     @IBOutlet private weak var tableView: UITableView!
 
@@ -24,55 +25,71 @@ final class MealsByCategoryViewController: UIViewController {
         tableView.dataSource = self
         tableView.delegate =  self
         loadMeal()
-        fetchFavorite()
+//        fetchFavorite()
     }
 }
 
 extension MealsByCategoryViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return meals.count
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return meals.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let meal = meals[indexPath.row]
+    let cell = UITableViewCell()
+    //        if let image = getImage(from: meal.thumb) {
+    //            cell.imageView?.image = image
+    //        }
+//    let urlImg = URL(string: meal.thumb)!
+//    guard cell.imageView?.image == nil else { return cell}
+//    cell.imageView?.downloaded(from: urlImg) {
+//      self.updateImageForCell(at: indexPath.row)
+//
+//    }
+    cell.textLabel?.text = meal.name
+    cell.imageView?.image = images[indexPath.row]
+    return cell
+  }
+
+  private func updateImageForCell(at index: Int) {
+    DispatchQueue.main.async { [self] in
+      let indexPath = IndexPath(row: index, section: 0)
+      if tableView.indexPathsForVisibleRows?.contains(indexPath) == true {
+        tableView.reloadRows(at: [indexPath], with: .fade)
+      }
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let meal = meals[indexPath.row]
-        let cell = UITableViewCell()
-//        if let image = getImage(from: meal.thumb) {
-//            cell.imageView?.image = image
-//        }
-        let urlImg = URL(string: meal.thumb)!
-            cell.imageView?.downloaded(from: urlImg)
-        cell.textLabel?.text = meal.name
-        return cell
-    }
-    
-    
-    private func loadMeal() {
-        guard let url = URL(string: "https://www.themealdb.com/api/json/v1/1/filter.php?c=\(categoryName)")
-        else { return }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        
-        URLSession.shared.dataTask(with: request) { responseData, response, error in
-            if let error = error {
-                print(error.localizedDescription)
-            } else if let jsonData = responseData {
-                let mealDataStore = try? JSONDecoder().decode(MealDataStore.self, from: jsonData)
-                DispatchQueue.main.async {
-                    self.meals = mealDataStore!.meals
-                }
-            }
-        }.resume()
-    }
-    
-    private func fetchFavorite() {
-        favorites = CoreDataService.shared.fetchRecipe()
-        if !favorites.isEmpty {
-            addedToFavoriteButton.image = UIImage(systemName: "bookmark.fill")
-        } else {
-            addedToFavoriteButton.image = UIImage(systemName: "bookmark")
+  }
+
+
+  private func loadMeal() {
+    guard let url = URL(string: "https://www.themealdb.com/api/json/v1/1/filter.php?c=\(categoryName)")
+    else { return }
+
+    var request = URLRequest(url: url)
+    request.httpMethod = "GET"
+
+    URLSession.shared.dataTask(with: request) { responseData, response, error in
+      if let error = error {
+        print(error.localizedDescription)
+      } else if let jsonData = responseData {
+        let mealDataStore = try? JSONDecoder().decode(MealDataStore.self, from: jsonData)
+        DispatchQueue.main.async {
+          self.meals = mealDataStore!.meals
         }
-    }
+      }
+    }.resume()
+  }
+
+  private func downloadImages(at index: Int) {
+    var meal = meals[index]
+    URLSession.shared.dataTask(with: URL(string: meal.thumb)!) { data, response, error in
+      guard  let data = data,
+        let image = UIImage(data: data)
+      else { return }
+      self.images[index] = image
+      self.updateImageForCell(at: index)
+    }.resume()
+  }
 }
 
 extension MealsByCategoryViewController: UITableViewDelegate {
@@ -86,11 +103,18 @@ extension MealsByCategoryViewController: UITableViewDelegate {
         }
         tableView.deselectRow(at: indexPath, animated: false)
     }
+
+  func tableView(_ tableView: UITableView,
+                 willDisplay cell: UITableViewCell,
+                 forRowAt indexPath: IndexPath) {
+    guard images[indexPath.row] == nil else { return }
+    downloadImages(at: indexPath.row)
+  }
 }
 
 extension MealsByCategoryViewController: UpdateFavoriteBtnDelegate {
     func updateFavoriteButton(favorite: [FavoriteViewModel]) {
-        
+
         if !favorite.isEmpty {
             addedToFavoriteButton.image = UIImage(systemName: "bookmark.fill")
         } else {
--- Recipes/Recipes/Recipes/ViewController.swift
+++ Recipes/Recipes/Recipes/ViewController.swift
@@ -9,26 +9,26 @@ import UIKit
 var categoryName = String()
 
 final class ViewController: UIViewController {
-    
+
     private var categories: [Category] = [] {
         didSet {
             tableView.reloadData()
         }
     }
-    
-    
-    
+
+
+
     @IBOutlet private weak var addedToFavoriteButton: UIBarButtonItem!
     @IBAction private func searchDidTap() {
         searchScreenAppear()
     }
-    
+
     @IBAction private func favoriteDidTap() {
         favoriteScreenAppear()
     }
-    
+
     @IBOutlet private weak var tableView: UITableView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         loadCategory()
@@ -36,42 +36,40 @@ final class ViewController: UIViewController {
         tableView.delegate =  self
         fetchFavorite()
     }
-    
+
     func searchScreenAppear() {
         let storyboard = UIStoryboard(name: "SearchVC", bundle: nil)
         if let vc = storyboard.instantiateViewController(withIdentifier: "SearchVC") as? SearchVC {
             navigationController?.pushViewController(vc, animated: true)
         }
     }
-    
+
     func favoriteScreenAppear() {
         let storyboard = UIStoryboard(name: "FavoriteList", bundle: nil)
         if let vc = storyboard.instantiateViewController(withIdentifier: "FavoriteList") as? FavoriteList {
             navigationController?.pushViewController(vc, animated: true)
         }
     }
-    
+
     private func fetchFavorite() {
         favorites = CoreDataService.shared.fetchRecipe()
-        
 
-        
         if !favorites.isEmpty {
             addedToFavoriteButton.image = UIImage(systemName: "bookmark.fill")
         } else {
             addedToFavoriteButton.image = UIImage(systemName: "bookmark")
         }
     }
-    
+
 
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categories.count
-        
+
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let category = categories[indexPath.row]
         let cell = UITableViewCell()
@@ -83,14 +81,14 @@ extension ViewController: UITableViewDataSource {
 //        cell.imageView?.downloaded(from: urlImg)
         return cell
     }
-    
+
     private func loadCategory() {
         guard let url = URL(string: "https://www.themealdb.com/api/json/v1/1/categories.php")
         else { return }
-        
+
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
-        
+
         URLSession.shared.dataTask(with: request) { responseData, response, error in
             if let error = error {
                 print(error.localizedDescription)
@@ -137,23 +135,26 @@ func getImage(from string: String?) -> UIImage? {
 
 
 extension UIImageView {
-    func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
-        contentMode = mode
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                //let mimeType = response?.mimeType, mimeType.hasSuffix("png"),
-                let data = data, error == nil,
-                let image = UIImage(data: data)
-                else { return }
-            DispatchQueue.main.async() { [weak self] in
-                self?.image = image
-            }
-        }.resume()
-    }
+  func downloaded(from url: URL,
+                  contentMode mode: ContentMode = .scaleAspectFit,
+                  completion: @escaping () -> Void) {
+    contentMode = mode
+    URLSession.shared.dataTask(with: url) { data, response, error in
+      guard
+        let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+        //let mimeType = response?.mimeType, mimeType.hasSuffix("png"),
+        let data = data, error == nil,
+        let image = UIImage(data: data)
+      else { return }
+      DispatchQueue.main.async() { [weak self] in
+        self?.image = image
+        completion()
+      }
+    }.resume()
+  }
     func downloaded(from link: String, contentMode mode: ContentMode = .scaleAspectFit) {
         guard let url = URL(string: link) else { return }
-        downloaded(from: url, contentMode: mode)
+      downloaded(from: url, contentMode: mode) {}
     }
 }
 
